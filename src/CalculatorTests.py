2ff1b807f2a8ff0a6bf01a8aef4779f9ee00af57
hs434 <hs434@njit.edu>
2019-11-02 19:01:35 -0400

    Task 10: In CalculatorTests file added functionality for DIVISION

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -3,6 +3,7 @@ __author__ = 'hs434'
 import unittest
 from Calculator import Calculator
 from CsvReader import CsvReader
+from decimal import getcontext, Decimal
 
 class MyTestCase(unittest.TestCase):
         def setUp(self) -> None:
@@ -33,6 +34,20 @@ class MyTestCase(unittest.TestCase):
             for row in testdata:
              self.assertEqual(self.calculator.multiply(row['Value 1'], row['Value 2']), int(row['Result']))
              self.assertEqual(self.calculator.result, int(row['Result']))
+
+        def test_division(self):
+            testdata = CsvReader('/src/Unit_Test_Division.csv').data
+            print(testdata)
+            for row in testdata:
+             if row['Value 1'] > row['Value 2']:
+                getcontext().prec = 8
+             else:
+                getcontext().prec = 10
+            self.assertEqual(self.calculator.divide(Decimal(row['Value 1']), Decimal(row['Value 2'])), Decimal(row['Result']))
+            self.assertEqual(self.calculator.result, Decimal(row['Result']))
+            pass
+
+
 
 if __name__ == '__main__':
     unittest.main()
